50ada00ba40bb00193dc349be8684616ef03c0eb
skeleton1231 <814010079@qq.com>
2023-11-25 15:16:05 +0800

    feat:add Facades methods

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -3,13 +3,15 @@ package log
 import (
 	"context"
 	"fmt"
+	"log"
+	"sync"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 type InfoLogger interface {
-	Info(msg string, fields ...zapcore.Field)
+	Info(msg string, fields ...Field)
 	Infof(format string, v ...interface{})
 	Infokv(msg string, kv ...interface{})
 	Enabled() bool
@@ -17,19 +19,19 @@ type InfoLogger interface {
 
 type Logger interface {
 	InfoLogger
-	Debug(msg string, fields ...zapcore.Field)
+	Debug(msg string, fields ...Field)
 	Debugf(format string, v ...interface{})
 	Debugkv(msg string, kv ...interface{})
-	Warn(msg string, fields ...zapcore.Field)
+	Warn(msg string, fields ...Field)
 	Warnf(format string, v ...interface{})
 	Warnkv(msg string, kv ...interface{})
-	Error(msg string, fields ...zapcore.Field)
+	Error(msg string, fields ...Field)
 	Errorf(format string, v ...interface{})
 	Errorkv(msg string, kv ...interface{})
-	Panic(msg string, fields ...zapcore.Field)
+	Panic(msg string, fields ...Field)
 	Panicf(format string, v ...interface{})
 	Panickv(msg string, kv ...interface{})
-	Fatal(msg string, fields ...zapcore.Field)
+	Fatal(msg string, fields ...Field)
 	Fatalf(format string, v ...interface{})
 	Fatalkv(msg string, kv ...interface{})
 
@@ -51,7 +53,7 @@ type infoLogger struct {
 }
 
 func (l *infoLogger) Enabled() bool { return true }
-func (l *infoLogger) Info(msg string, fields ...zapcore.Field) {
+func (l *infoLogger) Info(msg string, fields ...Field) {
 	if checkedEntry := l.log.Check(l.level, msg); checkedEntry != nil {
 		checkedEntry.Write(fields...)
 	}
@@ -94,12 +96,115 @@ func processFields(logger *zap.Logger, args []interface{}, additionalFields ...z
 	return append(fields, additionalFields...)
 }
 
+var (
+	logger = New(NewOptions())
+	mu     sync.Mutex
+)
+
+func Init(opts *Options) {
+	mu.Lock()
+	defer mu.Unlock()
+	logger = New(opts)
+}
+
+// New create logger by opts which can custmoized by command arguments.
+func New(opts *Options) *zapLogger {
+	if opts == nil {
+		opts = NewOptions()
+	}
+
+	var zapLevel zapcore.Level
+	if err := zapLevel.UnmarshalText([]byte(opts.Level)); err != nil {
+		zapLevel = zapcore.InfoLevel
+	}
+	encodeLevel := zapcore.CapitalLevelEncoder
+	// when output to local path, with color is forbidden
+	if opts.Format == consoleFormat && opts.EnableColor {
+		encodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+
+	encoderConfig := zapcore.EncoderConfig{
+		MessageKey:    "message",
+		LevelKey:      "level",
+		TimeKey:       "timestamp",
+		NameKey:       "logger",
+		CallerKey:     "caller",
+		StacktraceKey: "stacktrace",
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel:   encodeLevel,
+		EncodeCaller:  zapcore.ShortCallerEncoder,
+	}
+
+	loggerConfig := &zap.Config{
+		Level:             zap.NewAtomicLevelAt(zapLevel),
+		Development:       opts.Development,
+		DisableCaller:     opts.DisableCaller,
+		DisableStacktrace: opts.DisableStacktrace,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding:         opts.Format,
+		EncoderConfig:    encoderConfig,
+		OutputPaths:      opts.OutputPaths,
+		ErrorOutputPaths: opts.ErrorOutputPaths,
+	}
+
+	var err error
+	l, err := loggerConfig.Build(zap.AddStacktrace(zapcore.PanicLevel), zap.AddCallerSkip(1))
+	if err != nil {
+		panic(err)
+	}
+	logger := &zapLogger{
+		zapLogger: l.Named(opts.Name),
+		infoLogger: infoLogger{
+			log:   l,
+			level: zap.InfoLevel,
+		},
+	}
+	zap.RedirectStdLog(l)
+
+	return logger
+}
+
+// SugaredLogger returns a *zap.SugaredLogger instance, offering a more flexible logging approach.
+// This logger supports fast and loosely-typed logging, suitable for most use cases.
+func LoggerSugared() *zap.SugaredLogger {
+	return logger.zapLogger.Sugar()
+}
+
+// ErrLogger creates and returns a standard library *log.Logger instance that logs at the ErrorLevel in zap.
+// If the global logger is not initialized, it safely returns nil.
+func LoggerErr() *log.Logger {
+	if logger == nil {
+		return nil
+	}
+	if l, err := zap.NewStdLogAt(logger.zapLogger, zapcore.ErrorLevel); err == nil {
+		return l
+	}
+
+	return nil
+}
+
+// InfoLogger creates and returns a standard library *log.Logger instance that logs at the InfoLevel in zap.
+// If the global logger is not initialized, it returns nil.
+func LoggerInfo() *log.Logger {
+	if logger == nil {
+		return nil
+	}
+	if l, err := zap.NewStdLogAt(logger.zapLogger, zapcore.InfoLevel); err == nil {
+		return l
+	}
+
+	return nil
+}
+
 type zapLogger struct {
 	zapLogger *zap.Logger
 	infoLogger
 }
 
-func (l *zapLogger) Debug(msg string, fields ...zapcore.Field) {
+func (l *zapLogger) Debug(msg string, fields ...Field) {
 	l.zapLogger.Debug(msg, fields...)
 }
 
@@ -111,7 +216,7 @@ func (l *zapLogger) Debugkv(msg string, kv ...interface{}) {
 	l.zapLogger.Sugar().Debugw(msg, kv...)
 }
 
-func (l *zapLogger) Info(msg string, fields ...zapcore.Field) {
+func (l *zapLogger) Info(msg string, fields ...Field) {
 	l.zapLogger.Info(msg, fields...)
 }
 
@@ -123,7 +228,7 @@ func (l *zapLogger) Infokv(msg string, kv ...interface{}) {
 	l.zapLogger.Sugar().Infow(msg, kv...)
 }
 
-func (l *zapLogger) Warn(msg string, fields ...zapcore.Field) {
+func (l *zapLogger) Warn(msg string, fields ...Field) {
 	l.zapLogger.Warn(msg, fields...)
 }
 
@@ -135,7 +240,7 @@ func (l *zapLogger) Warnkv(msg string, kv ...interface{}) {
 	l.zapLogger.Sugar().Warnw(msg, kv...)
 }
 
-func (l *zapLogger) Error(msg string, fields ...zapcore.Field) {
+func (l *zapLogger) Error(msg string, fields ...Field) {
 	l.zapLogger.Error(msg, fields...)
 }
 
@@ -147,7 +252,7 @@ func (l *zapLogger) Errorkv(msg string, kv ...interface{}) {
 	l.zapLogger.Sugar().Errorw(msg, kv...)
 }
 
-func (l *zapLogger) Panic(msg string, fields ...zapcore.Field) {
+func (l *zapLogger) Panic(msg string, fields ...Field) {
 	l.zapLogger.Panic(msg, fields...)
 }
 
@@ -159,7 +264,7 @@ func (l *zapLogger) Panickv(msg string, kv ...interface{}) {
 	l.zapLogger.Sugar().Panicw(msg, kv...)
 }
 
-func (l *zapLogger) Fatal(msg string, fields ...zapcore.Field) {
+func (l *zapLogger) Fatal(msg string, fields ...Field) {
 	l.zapLogger.Fatal(msg, fields...)
 }
 
@@ -195,6 +300,27 @@ func (l *zapLogger) WithName(name string) Logger {
 	return &zapLogger{zapLogger: newLogger}
 }
 
+// Record extracts additional context (like request ID, username, etc.) from the provided context.Context
+// and returns a new logger instance with this context.
+func (l *zapLogger) Record(ctx context.Context) *zapLogger {
+	lg := l.clone()
+
+	if requestID := ctx.Value(KeyRequestID); requestID != nil {
+		lg.zapLogger = lg.zapLogger.With(zap.Any(KeyRequestID, requestID))
+	}
+	if username := ctx.Value(KeyUsername); username != nil {
+		lg.zapLogger = lg.zapLogger.With(zap.Any(KeyUsername, username))
+	}
+
+	return lg
+}
+
+func (l *zapLogger) clone() *zapLogger {
+	copy := *l
+
+	return &copy
+}
+
 func NewLogger(l *zap.Logger) Logger {
 	return &zapLogger{
 		zapLogger: l,
@@ -204,3 +330,88 @@ func NewLogger(l *zap.Logger) Logger {
 		},
 	}
 }
+
+// LoggerForLevel returns a logger for the specified logging level.
+func LoggerForLevel(level Level) InfoLogger { return logger.LoggerForLevel(level) }
+
+func Debug(msg string, fields ...Field) {
+	logger.zapLogger.Debug(msg, fields...)
+}
+
+func Debugf(format string, v ...interface{}) {
+	logger.zapLogger.Sugar().Debugf(format, v...)
+}
+
+func Debugw(msg string, kv ...interface{}) {
+	logger.zapLogger.Sugar().Debugw(msg, kv...)
+}
+
+func Info(msg string, fields ...Field) {
+	logger.zapLogger.Info(msg, fields...)
+}
+
+func Infof(format string, v ...interface{}) {
+	logger.zapLogger.Sugar().Infof(format, v...)
+}
+
+func Infow(msg string, kv ...interface{}) {
+	logger.zapLogger.Sugar().Infow(msg, kv...)
+}
+
+func Warn(msg string, fields ...Field) {
+	logger.zapLogger.Warn(msg, fields...)
+}
+
+func Warnf(format string, v ...interface{}) {
+	logger.zapLogger.Sugar().Warnf(format, v...)
+}
+
+func Warnw(msg string, kv ...interface{}) {
+	logger.zapLogger.Sugar().Warnw(msg, kv...)
+}
+
+func Error(msg string, fields ...Field) {
+	logger.zapLogger.Error(msg, fields...)
+}
+
+func Errorf(format string, v ...interface{}) {
+	logger.zapLogger.Sugar().Errorf(format, v...)
+}
+
+func Errorw(msg string, kv ...interface{}) {
+	logger.zapLogger.Sugar().Errorw(msg, kv...)
+}
+
+func Panic(msg string, fields ...Field) {
+	logger.zapLogger.Panic(msg, fields...)
+}
+
+func Panicf(format string, v ...interface{}) {
+	logger.zapLogger.Sugar().Panicf(format, v...)
+}
+
+func Panicw(msg string, kv ...interface{}) {
+	logger.zapLogger.Sugar().Panicw(msg, kv...)
+}
+
+func Fatal(msg string, fields ...Field) {
+	logger.zapLogger.Fatal(msg, fields...)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	logger.zapLogger.Sugar().Fatalf(format, v...)
+}
+
+func Fatalw(msg string, kv ...interface{}) {
+	logger.zapLogger.Sugar().Fatalw(msg, kv...)
+}
+
+func Record(ctx context.Context) *zapLogger {
+	return logger.Record(ctx)
+}
+
+func WithValues(kv ...interface{}) Logger { return logger.WithValues(kv...) }
+
+func WithName(s string) Logger { return logger.WithName(s) }
+
+func Flush() { logger.Sync() }
