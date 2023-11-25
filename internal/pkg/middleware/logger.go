07474ec3d501388aabe52ed7843009df17a8ed6e
skeleton1231 <814010079@qq.com>
2023-11-26 04:31:55 +0800

    fix

--- internal/pkg/middleware/logger.go
+++ internal/pkg/middleware/logger.go
@@ -1,12 +1,9 @@
 // Copyright 2023 Talhuang<talhuang1231@gmail.com>. All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
-
-// Package middleware provides middleware utilities for the Gin web framework.
 package middleware
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -14,158 +11,125 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/mattn/go-isatty"
-	"github.com/sirupsen/logrus"
+	"github.com/skeleton1231/gotal/pkg/log"
 )
 
-// defaultLogFormatter defines the default format for logging HTTP requests.
+// defaultLogFormatter is the default log format function Logger middleware uses.
 var defaultLogFormatter = func(param gin.LogFormatterParams) string {
-	// Returns the formatted log string.
-	return fmt.Sprintf("%3d - [%s] \"%v %s %s\" %s",
-		param.StatusCode,
+	var statusColor, methodColor, resetColor string
+	if param.IsOutputColor() {
+		statusColor = param.StatusCodeColor()
+		methodColor = param.MethodColor()
+		resetColor = param.ResetColor()
+	}
+
+	if param.Latency > time.Minute {
+		// Truncate in a golang < 1.8 safe way
+		param.Latency = param.Latency - param.Latency%time.Second
+	}
+
+	return fmt.Sprintf("%s%3d%s - [%s] \"%v %s%s%s %s\" %s",
+		// param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+		statusColor, param.StatusCode, resetColor,
 		param.ClientIP,
 		param.Latency,
-		param.Method,
+		methodColor, param.Method, resetColor,
 		param.Path,
 		param.ErrorMessage,
 	)
 }
 
-// Logger returns a middleware handler that logs HTTP requests.
+// Logger instances a Logger middleware that will write the logs to gin.DefaultWriter.
+// By default gin.DefaultWriter = os.Stdout.
 func Logger() gin.HandlerFunc {
-	// Returns a Logger with the default Gin logger configuration.
 	return LoggerWithConfig(GetLoggerConfig(nil, nil, nil))
 }
 
-// LoggerWithConfig takes a gin.LoggerConfig and returns a logging middleware.
+// LoggerWithFormatter instance a Logger middleware with the specified log format function.
+func LoggerWithFormatter(f gin.LogFormatter) gin.HandlerFunc {
+	return LoggerWithConfig(gin.LoggerConfig{
+		Formatter: f,
+	})
+}
+
+// LoggerWithWriter instance a Logger middleware with the specified writer buffer.
+// Example: os.Stdout, a file opened in write mode, a socket...
+func LoggerWithWriter(out io.Writer, notlogged ...string) gin.HandlerFunc {
+	return LoggerWithConfig(gin.LoggerConfig{
+		Output:    out,
+		SkipPaths: notlogged,
+	})
+}
+
+// LoggerWithConfig instance a Logger middleware with config.
 func LoggerWithConfig(conf gin.LoggerConfig) gin.HandlerFunc {
-	// Chooses which formatter to use for logging.
 	formatter := conf.Formatter
 	if formatter == nil {
 		formatter = defaultLogFormatter
 	}
 
-	// Determines the output destination for the logs.
 	out := conf.Output
 	if out == nil {
 		out = gin.DefaultWriter
 	}
 
-	// Checks if output should be colored.
-	isTerm := shouldUseColor(out)
+	notlogged := conf.SkipPaths
 
-	// Enables colored output if supported.
+	isTerm := true
+
+	if w, ok := out.(*os.File); !ok || os.Getenv("TERM") == "dumb" ||
+		(!isatty.IsTerminal(w.Fd()) && !isatty.IsCygwinTerminal(w.Fd())) {
+		isTerm = false
+	}
+
 	if isTerm {
 		gin.ForceConsoleColor()
 	}
 
-	// Creates a map of paths that should not be logged.
-	skip := skippedPaths(conf.SkipPaths)
+	var skip map[string]struct{}
 
-	// The middleware function that does the logging.
+	if length := len(notlogged); length > 0 {
+		skip = make(map[string]struct{}, length)
+
+		for _, path := range notlogged {
+			skip[path] = struct{}{}
+		}
+	}
+
 	return func(c *gin.Context) {
+		// Start timer
 		start := time.Now()
 		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
 
-		// Processes the request and calls the next handler.
+		// Process request
 		c.Next()
 
-		// Checks if the current path is not in the skip list.
+		// Log only when path is not being skipped
 		if _, ok := skip[path]; !ok {
-			// Gathers log parameters from the request.
-			param, contextFields := createLogFormatterParams(c, start, path)
-
-			// Marshal the context fields to a JSON string.
-			contextJSON, err := json.Marshal(contextFields)
-			if err != nil {
-				// Handle error, perhaps log that the context fields could not be marshaled.
-				logrus.WithError(err).Error("Could not marshal context fields")
+			param := gin.LogFormatterParams{
+				Request: c.Request,
+				Keys:    c.Keys,
 			}
 
-			logrus.WithFields(logrus.Fields{
-				"client_ip":     param.ClientIP,
-				"method":        param.Method,
-				"status_code":   param.StatusCode,
-				"latency":       param.Latency,
-				"user_agent":    param.Request.UserAgent(),
-				"error_message": param.ErrorMessage,
-				"body_size":     param.BodySize,
-				"path":          param.Path,
-				"context":       string(contextJSON), // Add the JSON string as the "context" field
-			}).Info("Request handled")
+			// Stop timer
+			param.TimeStamp = time.Now()
+			param.Latency = param.TimeStamp.Sub(start)
 
+			param.ClientIP = c.ClientIP()
+			param.Method = c.Request.Method
+			param.StatusCode = c.Writer.Status()
+			param.ErrorMessage = c.Errors.ByType(gin.ErrorTypePrivate).String()
+
+			param.BodySize = c.Writer.Size()
+
+			if raw != "" {
+				path = path + "?" + raw
+			}
+
+			param.Path = path
+
+			log.Record(c).Info(formatter(param))
 		}
 	}
 }
-
-// shouldUseColor determines if the log output should be colored based on the output writer.
-func shouldUseColor(out io.Writer) bool {
-	// Checks if the output is a terminal and if it supports color.
-	if w, ok := out.(*os.File); !ok || os.Getenv("TERM") == "dumb" || (!isatty.IsTerminal(w.Fd()) && !isatty.IsCygwinTerminal(w.Fd())) {
-		return false
-	}
-	return true
-}
-
-// skippedPaths creates a map from a list of paths to be skipped by the logger.
-func skippedPaths(notlogged []string) map[string]struct{} {
-	skip := make(map[string]struct{})
-	for _, path := range notlogged {
-		// Adds the path to the map to indicate it should be skipped.
-		skip[path] = struct{}{}
-	}
-	return skip
-}
-
-// createLogFormatterParams gathers information from the context to create log formatter parameters.
-func createLogFormatterParams(c *gin.Context, start time.Time, path string) (gin.LogFormatterParams, logrus.Fields) {
-	// Initializes the parameters structure with data from the request.
-	param := gin.LogFormatterParams{
-		Request: c.Request,
-		Keys:    c.Keys,
-	}
-
-	// Completes the parameter structure with additional timing and request info.
-	param.TimeStamp = time.Now()
-	param.Latency = param.TimeStamp.Sub(start)
-	param.ClientIP = c.ClientIP()
-	param.Method = c.Request.Method
-	param.StatusCode = c.Writer.Status()
-	param.ErrorMessage = c.Errors.ByType(gin.ErrorTypePrivate).String()
-	param.BodySize = c.Writer.Size()
-	param.Path = path + getRawQuery(c)
-
-	// Gather Logrus fields from the context.
-	logrusFields := logrus.Fields{}
-	if requestID, exists := c.Get("requestID"); exists {
-		logrusFields["requestID"] = requestID
-	}
-	if username, exists := c.Get("username"); exists {
-		logrusFields["username"] = username
-	}
-
-	return param, logrusFields
-}
-
-// getRawQuery appends the raw query string to the path if present.
-func getRawQuery(c *gin.Context) string {
-	raw := c.Request.URL.RawQuery
-	if raw != "" {
-		return "?" + raw
-	}
-	return ""
-}
-
-// // createLogEntry generates a structured logrus entry from the log formatter parameters.
-// func createLogEntry(param gin.LogFormatterParams) *logrus.Entry {
-// 	// Maps the log formatter parameters to a structured log entry.
-// 	return logrus.WithFields(logrus.Fields{
-// 		"client_ip":     param.ClientIP,
-// 		"method":        param.Method,
-// 		"status_code":   param.StatusCode,
-// 		"latency":       param.Latency,
-// 		"user_agent":    param.Request.UserAgent(),
-// 		"error_message": param.ErrorMessage,
-// 		"body_size":     param.BodySize,
-// 		"path":          param.Path,
-// 	})
-// }
